3d32ec06000f78a1e69c375e15ffbf22c773c122
Daniel Zacarias <danielzcrslima@gmail.com>
2026-03-02 19:40:50 -0300

    Novas mecânicas

--- Poo.py
+++ Poo.py
@@ -7,11 +7,19 @@ class Veiculo:
 		print("Bibibi")
 		
 class Moto(Veiculo):
+	def __init__(self, marca, modelo, cilindrada):
+		super().__init__(marca, modelo)
+		self.cilindrada = cilindrada
+			
 	def empinar(self):
 		print("Grau")
 		
+	def detalhes(self):
+		print(self.marca, self.modelo, self.cilindrada)
+		
 meu_carro = Veiculo("Honda", "Civic")
-minha_moto = Moto("Kawasaki", "ninja")
+minha_moto = Moto("Kawasaki", "ninja", "100")
 
 meu_carro.buzinar()
 minha_moto.empinar()
+minha_moto.detalhes()
